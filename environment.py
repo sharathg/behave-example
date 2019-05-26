759a0b1df9dc84148c9fc1fc45c8c172257a6bec
sharathg <sharath.g.pai@gmail.com>
2019-05-26 12:52:30 +0200

    Change geckodriver.log directory

--- environment.py
+++ environment.py
@@ -1,11 +1,12 @@
 from selenium.webdriver import FirefoxOptions
 from selenium.webdriver import Firefox
+from os import getcwd
 
 
 def before_all(context):
     options = FirefoxOptions()
     options.add_argument("--headless")
-    context.driver = Firefox(firefox_options=options)
+    context.driver = Firefox(firefox_options=options, service_log_path="{}/geckodriver.log".format(getcwd()))
 
 
 def after_all(context):
