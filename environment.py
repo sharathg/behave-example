1eabdf20f5d7d70b3f665c5c7f86cea70be53e40
sharathg <sharath.g.pai@gmail.com>
2019-05-26 13:05:28 +0200

    Push log to /tmp/

--- environment.py
+++ environment.py
@@ -1,12 +1,11 @@
 from selenium.webdriver import FirefoxOptions
 from selenium.webdriver import Firefox
-from os import getcwd
 
 
 def before_all(context):
     options = FirefoxOptions()
     options.add_argument("--headless")
-    context.driver = Firefox(firefox_options=options, service_log_path="{}/geckodriver.log".format(getcwd()))
+    context.driver = Firefox(firefox_options=options, service_log_path="/tmp/geckodriver.log")
 
 
 def after_all(context):
